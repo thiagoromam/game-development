ce3f4d5fb33855f5c7bb88c6da0cf0c8cfe2be42
Thiago Luis Romam Matheus <thiago.romam@outlook.com>
2015-03-30 18:24:30 -0300

    testing direct wpf+monogame interop

---
+++ MonogameWpf2/MonogameWpf2/Injection.cs
@@ -0,0 +1,14 @@
+﻿using Needles;
+
+namespace MonogameWpf2
+{
+    public class Injection
+    {
+        static Injection()
+        {
+            Container = NeedlesContainer.Create();
+        }
+
+        public static IContainer Container { get; private set; }
+    }
+}
